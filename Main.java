cf07a45e05dee7a3434b9116c701dfad7009c399
Keshav Garg <keshavgarg616@gmail.com>
2024-07-15 09:58:09 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import com.billsburgers.Meal;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -10,15 +12,15 @@ public class Main {
         regularMeal.addBurgerTopping("Olives", 0.15);
         System.out.println(regularMeal.getBill());
 
-        Meal deluxeMeal = new Meal(new Burger("Deluxe Burger", 10.00),
-                new Drink("Thums Up", "XL"),
-                new Side("Potato Wedges", 2.00));
+        Meal deluxeMeal = new Meal("Deluxe Burger", 10.00,
+                "Thums Up", "XL",
+                "Potato Wedges", 2.00);
         deluxeMeal.addBurgerTopping("Tomatoes", 0.10);
         deluxeMeal.addBurgerTopping("Onions", 0.10);
         deluxeMeal.addBurgerTopping("Cheese", 0.25);
         deluxeMeal.addBurgerTopping("Olives", 0.15);
         deluxeMeal.addBurgerTopping("Pickles", 0.05);
-//        deluxeMeal.addBurgerTopping("overloading", 10);
+        // deluxeMeal.addBurgerTopping("overloading", 10);
         System.out.println(deluxeMeal.getBill());
     }
 }
